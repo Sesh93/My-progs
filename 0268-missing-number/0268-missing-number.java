c7d2b8284b42b8568ec9b0f915d5bfc2a0173eb3
Seshu <133580636+Sesh93@users.noreply.github.com>
2024-06-27 09:48:10 +0530

    Time: 0 ms (100%), Space: 44.9 MB (74.38%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -4,7 +4,6 @@ class Solution {
         int sum=0;
         for(int i=0;i<nums.length;i++)
             sum+=nums[i];
-        Arrays.sort(nums);
         int n = nums.length;
         int key = n*(n+1)/2;
         return key-sum;    

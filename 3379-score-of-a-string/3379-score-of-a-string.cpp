f72faa1c4d3b04c90a2de1c97ea26f61415d5040
Seshu <133580636+Sesh93@users.noreply.github.com>
2024-06-28 19:08:19 +0530

    Time: 5 ms (26.67%), Space: 7.9 MB (41.11%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.cpp
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int cnt=0,i,n=s.length();
-        for(i=0;i<n-1;i++)
+        int cnt=0;
+        for(int i=0;i<s.length()-1;i++)
         {
             cnt+=abs(int(s[i])-int(s[i+1]));
         }

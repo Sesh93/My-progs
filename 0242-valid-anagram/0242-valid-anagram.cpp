a8a59425ee423e462a429991c867d6b58e85c1c5
Seshu <133580636+Sesh93@users.noreply.github.com>
2024-07-03 11:39:23 +0530

    Time: 5 ms (82.23%), Space: 8.6 MB (45.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -9,9 +9,9 @@ public:
             arr[s[i]-'a']++;
             arr[t[i]-'a']--;
         }
-        for(int i=0;i<s.size();i++)
+        for(int i=0;i<26;i++)
         {
-            if(arr[s[i]-'a']!=0)
+            if(arr[i]!=0)
                 return false;
         }
         return true;

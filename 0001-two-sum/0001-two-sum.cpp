c96cab99b471e744214fc3b55b33d0d4deaa6cc6
Seshu <133580636+Sesh93@users.noreply.github.com>
2024-07-05 13:41:22 +0530

    Time: 7 ms (84.65%), Space: 13.7 MB (41.85%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> m1;
+        vector<int> v1;
+        pair<int,int> x;
+        int key1=0,key2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            x.first=nums[i];
+            x.second=i;
+            m1.emplace_back(x);
+        }
+        sort(nums.begin(),nums.end());
+        int left=0,right=n-1;
+        while(left<right)
+        {
+            if(nums[left]+nums[right]<target)
+            {
+                left++;
+            }
+            else if(nums[left]+nums[right]>target)
+            {
+                right--;
+            }
+            else
+            {
+                key1=nums[left];
+                key2=nums[right];
+                break;
+            }
+        }
+        for(auto i:m1)
+        {
+            if(i.first==key1)
+                v1.emplace_back(i.second);
+            else if(i.first==key2)
+                v1.emplace_back(i.second);
+            else
+                continue;
+        }
+        return v1;
+    }
+};

edf7b7d9355b2ca0737f2f2445ba4199d57faded
Seshu <133580636+Sesh93@users.noreply.github.com>
2024-07-19 13:22:52 +0530

    Time: 11 ms (34.34%), Space: 21.3 MB (18.38%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int,int> ans;
-        int n=nums.size();
-        for(int i=0;i<n;i++)
-        {
-            ans[nums[i]]++;
-        }
-        vector<int> temp;
-        for(int i=0;i<n;i++)
-        {
-            if(ans[nums[i]]!=0)
-            {
-                ans[nums[i]]=0;
-                temp.emplace_back(nums[i]);
-            }
+        // unordered_map<int,int> ans;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++)
+        // {
+        //     ans[nums[i]]++;
+        // }
+        // vector<int> temp;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(ans[nums[i]]!=0)
+        //     {
+        //         ans[nums[i]]=0;
+        //         temp.emplace_back(nums[i]);
+        //     }
 
+        // }
+        // swap(temp,nums);
+        // return ans.size();
+        int n=nums.size();
+        int i=0;
+        for(int j=1;j<n;j++)
+        {
+            if(nums[i]!=nums[j])
+            {
+                i++;
+                nums[i]=nums[j];
+            }
         }
-        swap(temp,nums);
-        return ans.size();
+        return i+1;
     }
 };

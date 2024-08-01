02946f2a6ea5c798d52cc5a538021c71af3906bd
Seshu <133580636+Sesh93@users.noreply.github.com>
2024-08-01 10:44:33 +0530

    Time: 11 ms (98.98%), Space: 20.8 MB (30.71%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -5,16 +5,14 @@ public:
         sort(s.begin(),s.end());
         int i=0;
         int j=0;
-        int count=0;
         while(j<s.size()&&i<g.size())
         {
             if(g[i]<=s[j])
             {
-                count++;
                 i++;
             }
             j++;
         }
-        return count;
+        return i;
     }
 };
